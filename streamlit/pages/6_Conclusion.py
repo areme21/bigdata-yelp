9f19edb46cb2f629127be1bca16dfa62ef11469b
“areme21” <adamremels1@gmail.com>
2022-12-11 16:56:57 -0500

    conclude

--- streamlit/pages/6_Conclusion.py
+++ streamlit/pages/6_Conclusion.py
@@ -1,3 +1,7 @@
 import streamlit as st
 
-st.title("Conclusion")
+st.title("Conclusion/Next Steps")
+
+st.write("We recognize that there are inherent issues with Yelp data, such as fake reviews and the nature of an opt-in review platform that generally attracts reviews from the two extremes. However, given these limitations, we still believe that online review sites are important to customers in making decisions about a business, and should be important to businesses too. While other factors surely do influence business success that cannot be captured in this Yelp data, we believe it is an interesting perspective in exploring this question.")
+st.write("We also recognize that our model for predicting yelp score for prospective new businesses does not have perfect accuracy and would need to be improved upon before it is able to be deployed in a true business context.")
+st.write("We see potential for expanding our analysis to other cities, and comparing results between them. Urban characteristics, such as car usage, demographic breakdown, and overall culture seems to play a large effect in restaurant-customer relations. A deeper analysis into this would yield insightful results.")
